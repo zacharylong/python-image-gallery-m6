52e786aef311a71fcee0fea8ab9df43c19db5ac4
Zachary Long <zzl0100@auburn.edu>
2020-06-12 18:02:44 +0000

    Added functionality to store secrets in AWS and rotate them automatically

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -1,22 +1,40 @@
 import psycopg2
+from secrets import get_secret_image_gallery
+import json
 
-db_host = "demo-database-1.c923ckbw7nvl.us-east-2.rds.amazonaws.com"
-db_name = "image_gallery"
-db_user = "image_gallery"
+#db_host = "demo-database-1.c923ckbw7nvl.us-east-2.rds.amazonaws.com"
+#db_name = "image_gallery"
+#db_user = "image_gallery"
 
-password_file = "/home/ec2-user/.image_gallery_config"
+#password_file = "/home/ec2-user/.image_gallery_config"
 
 connection = None
 
-def get_password():
-    f = open(password_file, "r")
-    result = f.readline()
-    f.close()
-    return result[:-1]
+def get_secret():
+    jsonString = get_secret_image_gallery()
+    return json.loads(jsonString)
+
+def get_password(secret):
+    #f = open(password_file, "r")
+    #result = f.readline()
+    #f.close()
+    #return result[:-1]
+    return secret['password']
+
+def get_host(secret):
+    return secret['host']
+
+def get_username(secret):
+    return secret['username']
+
+def get_dbname(secret):
+    return secret['database_name']
 
 def connect():
     global connection
-    connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
+    secret = get_secret()
+    connection = psycopg2.connect(host=get_host(secret), dbname=get_dbname(secret), user=get_username(secret), password=get_password(secret))
+    #connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
 
 def execute(query,args=None):
     global connection
@@ -32,10 +50,10 @@ def main():
     res = execute('select * from users')
     for row in res:
         print(row)
-    res = execute("update users set password=%s where username='fred'", ('banana',))
-    res = execute('select * from users')
-    for row in res:
-        print(row)
+    #res = execute("update users set password=%s where username='fred'", ('banana',))
+    #res = execute('select * from users')
+    #for row in res:
+    #    print(row)
 
 if __name__ == '__main__':
     main()
