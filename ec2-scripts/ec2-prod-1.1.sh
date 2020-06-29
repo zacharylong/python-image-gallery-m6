11a00fed896ea719b298d76ec86077fa69af4d12
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-06-29 01:26:09 -0400

    Updated ec2-prod-1.1
    Changed github link that was correct in 1.0 to personal github for 1.1 too..

--- ec2-scripts/ec2-prod-1.1.sh
+++ ec2-scripts/ec2-prod-1.1.sh
@@ -10,7 +10,7 @@ amazon-linux-extras install -y nginx1
 
 # Configure/install custom software
 cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/python-image-gallery.git
+git clone https://github.com/zacharylong/python-image-gallery.git
 chown -R ec2-user:ec2-user python-image-gallery
 su ec2-user -l -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
 
