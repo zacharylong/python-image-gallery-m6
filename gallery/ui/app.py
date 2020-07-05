845df2cfda1128f04ebb0183712575c86915b917
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-07-05 16:54:34 -0400

    alternate upload from aws documentation

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -17,7 +17,8 @@ from .user import User
 from .postgres_user_dao import PostgresUserDAO
 from flask import flash
 import boto3
-from boto3.s3.key import Key
+import logging
+from botocore.exceptions import ClientError
 
 
 
@@ -241,30 +242,3 @@ def uploadImage():
 @app.route('/viewImages')
 def viewImages():
     return render_template('viewImages.html')
-
-@app.route('/upload2', methods=['GET', 'POST'])
-def index():
-    # If user has submitted the form...
-    if request.method == 'POST':
-
-        # Connect to Amazon S3
-        s3 = boto.connect_s3()
-
-        # Get a handle to the S3 bucket
-        bucket_name = 'zacs-m6-image-gallery'
-        bucket = s3.get_bucket(bucket_name)
-        k = Key(bucket)
-
-        # Loop over the list of files from the HTML input control
-        data_files = request.files.getlist('file[]')
-        for data_file in data_files:
-
-            # Read the contents of the file
-            file_contents = data_file.read()
-
-            # Use Boto to upload the file to the S3 bucket
-            k.key = data_file.filename
-            print("Uploading some data to " + bucket_name + " with key: " + k.key)
-            k.set_contents_from_string(file_contents)
-
-    return render_template('upload2.html')
