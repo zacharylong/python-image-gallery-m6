aeaecebc08dd9d54ce8fa926edafd82e17a4935a
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-07-05 19:54:02 -0400

    AWS called me to stop hard coding:

--- gallery/ui/s3config.py
+++ gallery/ui/s3config.py
@@ -1,10 +1,11 @@
 import os
+from .secrets import get_secret_s3_key, get_secret_s3_secret
 
 # hard coded from new IAM role for bucket only access
 
 #S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_BUCKET = "zacs-m6-image-gallery"
 #S3_KEY = os.environ.get("S3_KEY")
-S3_KEY = "AKIAYUWNHWSDPS6U6V72"
+S3_KEY = get_secret_s3_key()
 #S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
-S3_SECRET = "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"
+S3_SECRET = get_secret_s3_secret()
