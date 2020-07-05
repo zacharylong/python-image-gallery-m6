32471fda85ed4da40dcf1e61c7ad8d949dc66f33
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-07-05 13:39:21 -0400

    syntax, remove relative import

--- gallery/ui/postgres_user_dao.py
+++ gallery/ui/postgres_user_dao.py
@@ -1,7 +1,7 @@
 from .user_admin_m3 import get_secret, get_password, get_host, get_username, get_dbname, connect, execute
 from .user import User
 from .user_dao import UserDAO
-import user_admin_m3
+#import user_admin_m3
 
 class PostgresUserDAO(UserDAO):
     def __init__(self):
@@ -9,16 +9,16 @@ class PostgresUserDAO(UserDAO):
 
     def get_users(self):
         result = []
-        cursor = user_admin_m3.execute("select username,password,full_name from users")
+        cursor = execute("select username,password,full_name from users")
         for t in cursor.fetchall():
             result.append(User(t[0], t[1], t[2]))
         return result
 
     def delete_user(self, username):
-        user_admin_m3.execute("delete from users where username=%s", (username,))
+        execute("delete from users where username=%s", (username,))
 
     def get_user_by_username(self, username):
-        cursor = user_admin_m3.execute("select username,password,full_name from users where username=%s", (username,))
+        cursor = execute("select username,password,full_name from users where username=%s", (username,))
         row = cursor.fetchone()
         if row is None:
             return None
