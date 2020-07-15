14ce948d115f058652f28a1f3875374c1888d96a
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-07-14 22:18:33 -0400

    change to get env

--- gallery/ui/s3config.py
+++ gallery/ui/s3config.py
@@ -4,7 +4,7 @@ import os
 
 #S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_BUCKET = "zacs-m6-image-gallery"
-S3_KEY = os.environ.get("S3_KEY")
+S3_KEY = os.getenv("S3_KEY")
 #S3_KEY = get_secret_s3_key()
-S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+S3_SECRET = os.getenv("S3_SECRET_ACCESS_KEY")
 #S3_SECRET = get_secret_s3_secret()
