9a44391777d8257306d8df2eaeed442f6c02df61
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-07-14 21:48:38 -0400

    change s3 secrets to docker secrets

--- gallery/ui/s3config.py
+++ gallery/ui/s3config.py
@@ -5,7 +5,7 @@ from .secrets import get_secret_s3_key, get_secret_s3_secret
 
 #S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_BUCKET = "zacs-m6-image-gallery"
-#S3_KEY = os.environ.get("S3_KEY")
-S3_KEY = get_secret_s3_key()
-#S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
-S3_SECRET = get_secret_s3_secret()
+S3_KEY = os.environ.get("S3_KEY")
+#S3_KEY = get_secret_s3_key()
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+#S3_SECRET = get_secret_s3_secret()
